a005ecf988e1da575cf6c1df7a430a62a341f63c
hacker34 <johnny@hackinginnovatioins.com>
2017-03-05 16:40:19 -0700

    Installed Firebase & GeoFire
    Both firebase and geofire are loaded in the app and building with no
    errors

---
+++ PokeFinder/PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Johnny Hacking on 3/5/17.
+//  Copyright © 2017 HackingInnovations. All rights reserved.
+//
+
+#import "GeoFire.h"
--- PokeFinder/PokeFinder/ViewController.swift
+++ PokeFinder/PokeFinder/ViewController.swift
@@ -7,17 +7,81 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    //********Boiler Plate for Maps ****************
+    let locationManager = CLLocationManager()
+    var mapHasCenteredOnce = false
+    //**********************************************
+    //********Geofire Install variable *************
+    var geoFire: GeoFire!
+    //**********************************************
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //***Boiler Plate for Maps ***************
+        mapView.delegate = self
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+        //****************************************
     }
+    //*******Boiler Plate for Maps ****************
+    override func viewDidAppear(_ animated: Bool) {
+        locationAuthStatus()
+    }
+    
+    func locationAuthStatus() {
+        // Important to note that is only using location when app is in use not always!
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            mapView.showsUserLocation = true
+        }else{
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse{
+            mapView.showsUserLocation = true
+        }
+    }
+    
+    //This funciton would be used like with a button to recenter the map to basic location and zoom
+    func centerMapOnLocation(location: CLLocation){
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        // Map will only update the first time app is open, that way it allows user to pan map to look for other things
+        if let loc = userLocation.location{
+            if !mapHasCenteredOnce{
+                centerMapOnLocation(location: loc)
+                mapHasCenteredOnce = true
+            }
+        }
+    }
+    
+    //This function creates a custom annotation for the user on the map vrs just the blue dot!
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView: MKAnnotationView?
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named:"ash")
+        }
+        
+        return annotationView
+    }
+    
+    //********************************************
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func spotRandomPokemon(_ sender: UIButton) {
+        
     }
 
 
